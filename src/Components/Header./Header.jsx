cef5a45eeab3ef87c3248929eb0cf1d2d3daa4d6
Heir1 <nkeleheir@gmail.com>
2022-10-20 17:02:34 +0100

    end of google login

--- src/Components/Header./Header.jsx
+++ src/Components/Header./Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import youtube from '../../images/youtube.png'
 import profile0 from '../../images/profile.png'
 import { Container, Row, Col } from 'react-bootstrap';
 
 export const Header = () => {
+
+    const [profile, setProfile] = useState("");
+  
+    let account;
+  
+    // console.log(JSON.parse(localStorage.getItem("account"))[0]);
+    account = JSON.parse(localStorage.getItem("account"))[0];
+  
+    // console.log(localStorage.getItem("profile"));
+
+    useEffect(()=>{
+        if (localStorage.getItem('profile')){
+          
+        } else {      
+          const fetchData = () => {
+            fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&id=${account}&key=AIzaSyAxpjZGN2NGfGfa3djYxcOt7Mx6GOq4quU`)
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              // setProfile(data.items[0].snippet.thumbnails.high.url)
+              localStorage.setItem("profile", JSON.stringify(data.items[0].snippet.thumbnails.high.url))
+            })
+          }
+          fetchData();
+        }
+    
+      }, [])
+
   return (
     <React.Fragment>
         
@@ -30,7 +58,7 @@ export const Header = () => {
                             
                         </div>
                         <div className="col-md-4 col-sm-4 col-xs-4">
-                            <img src={profile0} alt="" className="profile"/>
+                            <img src={ localStorage.getItem('profile') ? JSON.parse(localStorage.getItem('profile')) : profile0} className="profile" />
                         </div>
                     </div>   
                 </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import { Header } from '../Header./Header'
 import { Siderbar } from '../Siderbar/Siderbar'
 import { Homevideo } from './Homevideo'
 
 export const Home = () => {
+
+  let channel;
+
+  if(localStorage.getItem("channels")){{
+    channel = JSON.parse(localStorage.getItem('channels'))[0][3].snippet.resourceId.channelId;
+    console.log(JSON.parse(localStorage.getItem('channels'))[0][3].snippet.resourceId.channelId);
+  }}
+  
+  
+  const [loading, setLoading] = useState(false)
+  const [videoFilter, setVideoFilter] = useState("");
+  const [videos, setVideos] = useState([]);
+  
+
+  useEffect(() => {
+      setLoading(true);
+      const fetchData = async () => {
+        const results = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel}&maxResults=20&order=date&type=video&key=AIzaSyAxpjZGN2NGfGfa3djYxcOt7Mx6GOq4quU`)
+
+
+        setVideos(results.data.items)
+
+        console.log(results.data);
+        setLoading(false);
+      }
+
+    fetchData()
+
+  }, [])
+
   return (
     <React.Fragment>
-        <Header/>
+        <Header setVideoFilter={setVideoFilter} />
         <div className="container-fluid">
             <div className="row">
                 <Siderbar/>
